686dc23920db6056e518a7d9102b5a813c9974f8
Shivi-Codes <shivania.govender@gmail.com>
2025-04-04 21:40:30 +0200

    Custom JavaScript Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 console.log('javascript connected!');
     
 const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
+    interval: 5000,
     pause: false
+})
+
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
 })
 
 // when the pause button is clicked, pause the carousel
